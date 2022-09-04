ec8614a94f286cb0a3c3fb44ee5b8e1c3a1cb903
egijatiramadhan <egijatir@gmail.com>
2022-09-04 22:57:14 +0800

    dark mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["index.html"],
+  content: ['index.html'],
+  darkMode: 'class',
   theme: {
-    container:{
-      center:true,
-      padding:'16px',
+    container: {
+      center: true,
+      padding: '16px',
     },
-    extend: {},
-  },
-  screens:{
-    '2xl':'1320px'
+    extend: {
+      colors: {
+        primary: '#14b8a6',
+        secondary: '#64748b',
+        dark: '#0f172a',
+      },
+      screens: {
+        '2xl': '1320px',
+      },
+    },
   },
   plugins: [],
-}
+};
